fc29bed588c704ad780bc0458441173f3b58c178
JEEVAN S <chuppijeevan@gmail.com>
2023-11-07 23:23:52 +0530

    Create gcdrecursion.py

---
+++ gcdrecursion.py
@@ -0,0 +1,9 @@
+
+def gcd(num1,num2):
+    if num2 == 0:
+        return num1;
+    return gcd(num2, num1 % num2)
+#taking inputs from the user
+num1 = int(input("Enter first number: "))  
+num2 = int(input("Enter second number: "))  
+print("hcf/gcd of",num1,"and",num2,"=",gcd(num1,num2))
