afe3b8796cd69395910606a174742bf205c3e929
JEEVAN S <chuppijeevan@gmail.com>
2023-11-08 23:52:56 +0530

    Create POW.py

---
+++ POW.py
@@ -0,0 +1,8 @@
+base = int(input("Enter the value for base :"))
+exponent = int(input("Enter the value for exponent :"))
+result=1;
+print(base,"to power ",exponent,"=",end = ' ')
+#using ‘for’ loop with ‘range’ function
+for exponent in range(exponent, 0, -1):
+    result *= base
+print(result)
